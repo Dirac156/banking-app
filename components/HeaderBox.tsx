a020b1b1605a31802b0e4099658df835ef90c9d1
dirac156 <dmurairimukongya@gmail.com>
2024-08-30 22:32:27 +0200

    implemented header box

---
+++ components/HeaderBox.tsx
@@ -0,0 +1,18 @@
+export default function HeaderBox({
+  type,
+  title,
+  user,
+  subtext,
+}: HeaderBoxProps) {
+  return (
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === "greeting" && (
+          <span className="text-bankGradient"> {user}</span>
+        )}
+      </h1>
+      <p className="header-box-subtext">{subtext}</p>
+    </div>
+  );
+}
